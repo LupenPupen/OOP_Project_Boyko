633a87ccbff59d27e60278e47713d1d46f7233a8
LupenPupen <litandrey237@gmail.com>
2025-05-20 17:42:34 +0300

    Добавьте файлы проекта.

---
+++ OOP_Project_Boyko/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace OOP_Project_Boyko
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
