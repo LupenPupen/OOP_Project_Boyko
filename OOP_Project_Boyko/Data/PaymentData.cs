adfd4c226022a394710f8a62c367224a233ace17
LupenPupen <litandrey237@gmail.com>
2025-05-22 01:01:31 +0300

    Comit: Fix

--- OOP_Project_Boyko/Data/PaymentData.cs
+++ OOP_Project_Boyko/Data/PaymentData.cs
@@ -8,11 +8,11 @@ namespace OOP_Project_Boyko.Data
 {
     public class PaymentData
     {
-        public string FullName { get; set; }
-        public string PhoneNumber { get; set; }
-        public string CardNumber { get; set; }
-        public string ExpiryDate { get; set; }
-        public string Cvv { get; set; }
+        public string? FullName { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? CardNumber { get; set; }
+        public string? ExpiryDate { get; set; }
+        public string? Cvv { get; set; }
         public int Hours { get; set; }
     }
 }
